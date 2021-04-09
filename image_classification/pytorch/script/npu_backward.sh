1bf07efc256ecbf86ed5211ce194b4c1aa76eb80
luweizheng <luweizheng36@hotmail.com>
2021-04-09 18:00:13 +0800

    log

--- image_classification/pytorch/script/npu_backward.sh
+++ image_classification/pytorch/script/npu_backward.sh
@@ -15,16 +15,22 @@ do
     for batch_size in 64 128 256
     do
         filename=${compute_type}-${arch}-bs_${batch_size}
-        echo $filename
-        python3 train.py --platform ${platform} \
-                    --compute_type ${compute_type} \
-                    --arch ${arch} \
-                    --dtype ${dtype} \
-                    --num_iterations 100 \
-                    --amp \
-                    --opt-level "O2" \
-                    --input_tensor_shape ${batch_size} 3 224 224 \
-                    1>./output/${platform}/$filename 2>&1 &
-        wait
+        if [ -e ./output/${platform}/${filename} ]
+        then
+            echo $filename
+            echo "exist"
+        else
+            echo $filename
+            python3 bench.py --platform ${platform} \
+                        --compute_type ${compute_type} \
+                        --arch ${arch} \
+                        --dtype ${dtype} \
+                        --num_iterations 100 \
+                        --amp \
+                        --opt-level "O2" \
+                        --input_tensor_shape ${batch_size} 3 224 224 \
+                        1>./output/${platform}/$filename 2>&1 &
+            wait
+        fi
     done
 done
