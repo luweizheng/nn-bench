06b3492a27e006d77fca86888c2ac3a2fe70ad7f
luweizheng <luweizheng36@hotmail.com>
2021-04-19 23:34:25 +0800

    rescale cifar10

--- image_classification/pytorch/train_1p_cifar10.py
+++ image_classification/pytorch/train_1p_cifar10.py
@@ -18,7 +18,8 @@ import torch.utils.data
 import torch.utils.data.distributed
 import torchvision.transforms as transforms
 import torchvision.datasets as datasets
-import resnet_cifar10 as models
+import torchvision.models as models
+# import resnet_cifar10 as models
 from torch.utils.tensorboard import SummaryWriter
 
 from apex import amp
@@ -183,13 +184,14 @@ def main_worker(args):
     # Data loading code
     train_sampler = None
 
-    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                     std=[0.229, 0.224, 0.225])
+    normalize = transforms.Normalize(mean=[0.4914, 0.4822, 0.4465],
+                                     std=[0.2023, 0.1994, 0.2010])
 
     train_loader = torch.utils.data.DataLoader(
         datasets.CIFAR10(root=args.data, train=True, transform=transforms.Compose([
             transforms.RandomHorizontalFlip(),
             transforms.RandomCrop(32, 4),
+            transforms.Resize(224),
             transforms.ToTensor(),
             normalize,
         ]), download=True),
@@ -198,6 +200,7 @@ def main_worker(args):
 
     val_loader = torch.utils.data.DataLoader(
         datasets.CIFAR10(root=args.data, train=False, transform=transforms.Compose([
+            transforms.Resize(224),
             transforms.ToTensor(),
             normalize,
         ])),
